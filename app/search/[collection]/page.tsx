5201c81bfc3c3243b2d96b25c71ab4d8449f388b
yusufom <yusufolamilekanmuktar@gmail.com>
2024-05-25 23:24:09 +0100

    collection

--- app/search/[collection]/page.tsx
+++ app/search/[collection]/page.tsx
@@ -1,26 +1,28 @@
-import { getCollection, getCollectionProducts } from 'lib/shopify';
-import { Metadata } from 'next';
-import { notFound } from 'next/navigation';
+/* eslint-disable no-unused-vars */
+// import { getCollection, getCollectionProducts } from 'lib/shopify';
+// import { Metadata } from 'next';
+// import { notFound } from 'next/navigation';
 
 import Grid from 'components/grid';
 import ProductGridItems from 'components/layout/product-grid-items';
 import { defaultSort, sorting } from 'lib/constants';
+import { homepageItems } from 'lib/data';
 
-export async function generateMetadata({
-  params
-}: {
-  params: { collection: string };
-}): Promise<Metadata> {
-  const collection = await getCollection(params.collection);
+// export async function generateMetadata({
+//   params
+// }: {
+//   params: { collection: string };
+// }): Promise<Metadata> {
+//   const collection = await getCollection(params.collection);
 
-  if (!collection) return notFound();
+//   if (!collection) return notFound();
 
-  return {
-    title: collection.seo?.title || collection.title,
-    description:
-      collection.seo?.description || collection.description || `${collection.title} products`
-  };
-}
+//   return {
+//     title: collection.seo?.title || collection.title,
+//     description:
+//       collection.seo?.description || collection.description || `${collection.title} products`
+//   };
+// }
 
 export default async function CategoryPage({
   params,
@@ -29,9 +31,10 @@ export default async function CategoryPage({
   params: { collection: string };
   searchParams?: { [key: string]: string | string[] | undefined };
 }) {
-  const { sort } = searchParams as { [key: string]: string };
-  const { sortKey, reverse } = sorting.find((item) => item.slug === sort) || defaultSort;
-  const products = await getCollectionProducts({ collection: params.collection, sortKey, reverse });
+  // const { sort } = searchParams as { [key: string]: string };
+  // const { sortKey, reverse } = sorting.find((item) => item.slug === sort) || defaultSort;
+  // const products = await getCollectionProducts({ collection: params.collection, sortKey, reverse });
+  const products = homepageItems;
 
   return (
     <section>
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,7 +1,8 @@
 import Grid from 'components/grid';
 import ProductGridItems from 'components/layout/product-grid-items';
-import { defaultSort, sorting } from 'lib/constants';
-import { getProducts } from 'lib/shopify';
+// import { defaultSort, sorting } from 'lib/constants';
+import { homepageItems } from 'lib/data';
+// import { getProducts } from 'lib/shopify';
 
 export const metadata = {
   title: 'Search',
@@ -13,10 +14,11 @@ export default async function SearchPage({
 }: {
   searchParams?: { [key: string]: string | string[] | undefined };
 }) {
+  // eslint-disable-next-line no-unused-vars
   const { sort, q: searchValue } = searchParams as { [key: string]: string };
-  const { sortKey, reverse } = sorting.find((item) => item.slug === sort) || defaultSort;
+  // const { sortKey, reverse } = sorting.find((item) => item.slug === sort) || defaultSort;
 
-  const products = await getProducts({ sortKey, reverse, query: searchValue });
+  const products = homepageItems;
   const resultsText = products.length > 1 ? 'results' : 'result';
 
   return (
